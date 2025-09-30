efe3407b50c23439244078273cb2772bf026e9c6
Parvez4560 <salafipay@gmail.com>
2025-09-30 11:02:46 +0600

    Updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
-const initBalances = require('../utils/initBalances');
+const { initBalances } = require('../utils/initBalances');
 const validateDocuments = require('../utils/validateDocuments');
 
 // Generate temporary PIN
