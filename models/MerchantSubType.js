7c1caaa5be3894c7ff38505a113708eafe4b9f0f
Parvez4560 <salafipay@gmail.com>
2025-10-02 11:37:13 +0600

    Updated

--- models/MerchantSubType.js
+++ models/MerchantSubType.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const merchantTypeSchema = new mongoose.Schema({
+const merchantSubTypeSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },   // যেমন: Shop, NGO, Education, E-commerce
   description: { type: String, default: "" },             
   status: { type: String, enum: ["active", "inactive"], default: "active" }
 }, { timestamps: true });
 
-module.exports = mongoose.model('MerchantSubType', merchantTypeSchema);
+module.exports = mongoose.model('MerchantSubType', merchantSubTypeSchema);
